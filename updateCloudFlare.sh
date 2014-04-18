aa170144b7d220fcd20f180816c09f7ff3b82dd2
Scott Helme <scotthelme@hotmail.com>
2014-04-18 10:30:13 +0100

    Minor change to instructions.

--- updateCloudFlare.sh
+++ updateCloudFlare.sh
@@ -1,1 +1,1 @@
-wget -qO- http://www.example.com/updateCloudFlare.php?auth=***YourTokenHere*** &> /dev/null
+wget -qO- http://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here*** &> /dev/null
