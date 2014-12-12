fa5df993c57239e3c648a76c9f904c4124bb0716
Th3M4st3r <Th3M4st3r@users.noreply.github.com>
2014-12-12 18:42:27 +0200

    Update updateCloudFlare.sh
    Force wget to connect with ipv4/ipv6 so the A/AAAA record can be updated.
    (if both the client and the server have ipv4 and/or ipv6 connectivity and the appropriate dns record exists)

--- updateCloudFlare.sh
+++ updateCloudFlare.sh
@@ -1,1 +1,2 @@
-wget -qO- http://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here*** &> /dev/null
+wget -4qO- http://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here*** &> /dev/null
+wget -6qO- http://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here*** &> /dev/null
