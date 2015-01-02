cb343c5bdb6d8218e39a901b1081172e8e3bbaa3
Th3M4st3r <Th3M4st3r@users.noreply.github.com>
2015-01-02 22:24:54 +0200

    Added automatic creation of missing DNS records

--- updateCloudFlare.php
+++ updateCloudFlare.php
@@ -1,7 +1,7 @@
 <?php
 
-// A key can be any alphanumeric string
-// Insert the appropriate "key" => "hostname" values below
+// A key can be any alphanumeric string.
+// Insert the appropriate "key" => "hostname" values below.
 $hosts = array(
 	"***Insert Random Key1 Here***" => "ddns1",
 	"***Insert Random Key2 Here***" => "ddns2",
@@ -18,13 +18,12 @@ $myDomain	= "example.com";				// Your domain name.
 $emailAddress	= "CloudFlareAccountEmailAddress";		// The email address of your CloudFlare account.
 
 // These values do not need to be changed.
-$ddnsAddress	= $hosts[$_GET['auth']].".".$myDomain;		// The subdomain that will be updated.
+$hostname	= $hosts[$_GET['auth']];			// The hostname that will be updated.
+$ddnsAddress	= $hostname.".".$myDomain;			// The fully qualified domain name.
 $ip		= $_SERVER['REMOTE_ADDR'];			// The IP of the client calling the script.
-$id		= 0;						// The CloudFlare ID of the subdomain, used later.
 $url		= 'https://www.cloudflare.com/api_json.html';	// The URL for the CloudFlare API.
-$cfIP		= '';						// The IP Cloudflare has for the subdomain.
 
-// Sends request to CloudFlare and returns the response
+// Sends request to CloudFlare and returns the response.
 function send_request() {
 	global $url, $fields;
 
@@ -62,18 +61,37 @@ $fields = array(
 
 $result = send_request();
 
-// Extract the record ID for the subdomain we want to update.
+// Extract the record ID (if it exists) for the subdomain we want to update.
+$rec_exists = False;						// Assume that the record doesn't exist.
 $data = json_decode($result);
 foreach($data->response->recs->objs as $rec){
 	if(($rec->name == $ddnsAddress) && ($rec->type == $type)){
+		$rec_exists = True;				// If this runs, it means that the record exists.
 		$id = $rec->rec_id;
-		$cfIP = $rec->content;
+		$cfIP = $rec->content;				// The IP Cloudflare has for the subdomain.
 		break;
 	}
 }
 
+// Create a new record if it doesn't exist
+if(!$rec_exists){
+	// Build the request to create a new DNS record.
+	// https://www.cloudflare.com/docs/client-api.html#s5.1
+	$fields = array(
+		'a' => urlencode('rec_new'),
+		'tkn' => urlencode($apiKey),
+		'email' => urlencode($emailAddress),
+		'z' => urlencode($myDomain),
+		'type' => urlencode($type),
+		'name' => urlencode($hostname),
+		'content' => urlencode($ip),
+		'ttl' => urlencode ('1')
+	);
+
+	$result = send_request();
+	
 // Only update the entry if the IP addresses do not match.
-if ($ip != $cfIP){
+} elseif($ip != $cfIP){
 	// Build the request to update the DNS record with our new IP.
 	// https://www.cloudflare.com/docs/client-api.html#s5.2
 	$fields = array(
@@ -83,7 +101,7 @@ if ($ip != $cfIP){
 		'email' => urlencode($emailAddress),
 		'z' => urlencode($myDomain),
 		'type' => urlencode($type),
-		'name' => urlencode($ddnsAddress),
+		'name' => urlencode($hostname),
 		'content' => urlencode($ip),
 		'service_mode' => urlencode('0'),
 		'ttl' => urlencode ('1')
