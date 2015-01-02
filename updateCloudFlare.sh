5e5c611005e822d6b88172f8a68adba8806000dd
Th3M4st3r <Th3M4st3r@users.noreply.github.com>
2015-01-02 14:50:30 +0200

    Improved flexibility

--- updateCloudFlare.sh
+++ updateCloudFlare.sh
@@ -1,2 +1,14 @@
-wget -4qO- http://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here*** &> /dev/null
-wget -6qO- http://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here*** &> /dev/null
+#!/bin/bash
+
+########## Edit the following lines ##########
+IPv4=true
+IPv6=false
+URL="https://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here***"
+##############################################
+
+if $IPv4; then
+  wget -4qO- $URL &> /dev/null
+fi
+if $IPv6; then
+  wget -6qO- $URL &> /dev/null
+fi
