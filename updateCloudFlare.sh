d902b8aeeb005a171602fa19c590d0ebf635f5bf
AEtherC0r3 <master@aetherc0r3.eu>
2016-07-27 12:44:23 +0300

    Use POST instead of GET requests

--- updateCloudFlare.sh
+++ updateCloudFlare.sh
@@ -4,13 +4,14 @@
 IPv4=true
 IPv6=false
 Logfile="/var/log/ddns.log"
-URL="https://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here***"
+URL="https://www.example.com/updateCloudFlare.php"
+Token="***Insert Your Key Here***"
 ##############################################
 
 date >> $Logfile
 if $IPv4; then
-  wget -4qO- $URL >> $Logfile
+  wget -4qO- --post-data="auth=$Token" $URL >> $Logfile
 fi
 if $IPv6; then
-  wget -6qO- $URL >> $Logfile
+  wget -6qO- --post-data="auth=$Token" $URL >> $Logfile
 fi
